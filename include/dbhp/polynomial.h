1413c41def746b4005c5f6ca851d7e0a20920470
Dillon Huff <dillonhuff@gmail.com>
2016-06-11 10:26:42 -0700

    Building out skeleton of polynomial class

--- include/dbhp/polynomial.h
+++ include/dbhp/polynomial.h
@@ -1,20 +1,79 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 
 namespace dbhp {
 
   template<typename T>
-  class monomial {};
-
-  template<typename T>
-  class polynomial {
+  class monomial {
+  protected:
+    T coeff;
+    std::vector<unsigned> pows;
+    
   public:
-    polynomial(const std::vector<monomial<T>>& ) {}
+    monomial(const T p_coeff, const std::vector<unsigned>& p_pows) :
+      coeff(p_coeff), pows(p_pows) {}
+
+    monomial<T> scalar_times(const T c) const {
+      return monomial<T>(c*coeff, pows);
+    }
   };
 
   template<typename T>
-  bool operator==(const polynomial<T>&, const polynomial<T>&) {
-    return true;
+  class polynomial {
+  protected:
+    std::vector<monomial<T>> monomials;
+
+  public:
+    polynomial(const std::vector<monomial<T>>& p_monomials) :
+      monomials(p_monomials) {}
+
+    polynomial<T> times(const polynomial<T>& other) const {
+      unsigned this_ind = 0;
+      unsigned this_size = monomials.size();
+
+      unsigned other_ind = 0;
+      unsigned other_size = other.monomials.size();
+
+      std::vector<monomial<T>> new_monos;
+
+      while (this_ind < this_size && other_ind < other_size) {
+	this_ind++;
+	other_ind++;
+      }
+
+      return polynomial<T>(new_monos);
+    }
+
+    bool equals(const polynomial<T>& other) const {
+      return true;
+    }
+  };
+
+  template<typename T>
+  monomial<T> operator*(const T c, const monomial<T>& m) {
+    return m.scalar_times(c);
   }
+
+  template<typename T>
+  polynomial<T> operator+(const polynomial<T>& p, const polynomial<T>& q) {
+    return p.times(q);
+  }
+
+  template<typename T>
+  bool operator==(const polynomial<T>& p, const polynomial<T>& q) {
+    return p.equals(q);
+  }
+
+  template<typename T>
+  bool operator!=(const polynomial<T>& p, const polynomial<T>& q) {
+    return !(p == q);
+  }
+
+  // template<typename T>
+  // std::ostream operator<<(std::ostream&, const polynomial<T>&) {
+  //   assert(false);
+  // }
+
 }
--- test/polynomial_tests.cpp
+++ test/polynomial_tests.cpp
@@ -17,5 +17,34 @@ namespace dbhp {
     REQUIRE(p == q);
   }
 
-  
+  TEST_CASE("Compare different polynomials") {
+    vector<monomial<unsigned>> ms{};
+    polynomial<unsigned> q(ms);
+
+    vector<unsigned> powers{3, 3};
+
+    monomial<unsigned> m1(5, powers);
+    vector<monomial<unsigned>> ms1{m1};
+    polynomial<unsigned> p(ms1);
+
+    REQUIRE(p != q);
+  }
+
+  TEST_CASE("Add int polynomials") {
+    vector<unsigned> powers1{1, 3, 2, 0};
+    vector<unsigned> powers2{1, 5, 6, 1};
+
+    monomial<int> m1(5, powers1);
+    monomial<int> m2(-3, powers2);
+    vector<monomial<int>> ms1{m1, m2};
+    vector<monomial<int>> ms2{m1};
+
+    vector<monomial<int>> s{2*m1, m2};
+    polynomial<int> sum(s);
+    
+    polynomial<int> p(ms1);
+    polynomial<int> q(ms2);
+    
+    REQUIRE(sum == p + q);
+  }
 }
